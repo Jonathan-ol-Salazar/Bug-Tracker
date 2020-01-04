231076ff66bd62fcb8d4cbe6c113757f4016edb8
SALAZAR <jonathan-ol-salazar@outlook.com>
2020-01-04 13:10:32 +1000

    Adding .NET Standard Class Library

---
+++ BugTrackerDataAccess/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace BugTrackerDataAccess.Models
+{
+    public class User
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        
+        [BsonElement]
+        [Required]
+        public int UserID { get; set; }
+
+        [BsonElement]
+        [Required]
+        public string UserName { get; set; }
+
+        [BsonElement]
+        [Required]
+        public string Email { get; set; }
+
+        [BsonElement]
+        [Required]
+        public string Role { get; set; }
+    }
+}
