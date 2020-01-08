0ab80e9f7785802bc0f151e2520004c797cf7e01
SALAZAR <jonathan-ol-salazar@outlook.com>
2020-01-09 00:46:28 +1000

    Default role 'Submitter', is set when user login has no role

--- Bug Tracker/Controllers/HomeController.cs
+++ Bug Tracker/Controllers/HomeController.cs
@@ -65,42 +65,28 @@ namespace Bug_Tracker.Controllers
 
 
                 // GETTING ROLES ASSIGNED TO USER FROM AUTH0
-
                 // Format: https://wussubininja.au.auth0.com/api/v2/users/USER_ID/roles
-
-
-                //string baseURL = "https://wussubininja.au.auth0.com/api/v2/users/" + user_ID + "/roles";
-                //string authorizationValue = "Bearer " + Auth0ManagementAPI_AccessToken;
-                //// Endpoint to get user role
-                //client = new RestClient(baseURL);
-                //request = new RestRequest(Method.GET);
-                //// Add Auth0 Management API Access Token 
-                //request.AddHeader("authorization", authorizationValue);
-                //response = client.Execute(request);
-
-                //// Remove the '[' and ']' from response string so it can be converted into JSON
-                //response2dict = JObject.Parse(response.Content.TrimStart('[').TrimEnd(']'));
-                //// Retrieving role assigned through Auth0
-                //var role = response2dict.SelectToken("name").ToString();
-
-
-
-                // Setting Role 
+                
                 string baseURL = "https://wussubininja.au.auth0.com/api/v2/users/" + user_ID + "/roles";
                 string authorizationValue = "Bearer " + Auth0ManagementAPI_AccessToken;
                 // Endpoint to get user role
                 client = new RestClient(baseURL);
-                request = new RestRequest(Method.POST);
-                request.AddHeader("content-type", "application/json");
+                request = new RestRequest(Method.GET);
+                // Add Auth0 Management API Access Token 
                 request.AddHeader("authorization", authorizationValue);
-                request.AddHeader("cache-control", "no-cache");
-                request.AddParameter("application/json", "{ \"roles\": [ \"ROLE_ID\" ] }", ParameterType.RequestBody);
                 response = client.Execute(request);
 
 
-                var x = response;
-
-
+                // If no role is set, 'Submitter' role will be selected
+                if (response.ContentLength == -1)
+                {
+                    request = new RestRequest(Method.POST);
+                    request.AddHeader("content-type", "application/json");
+                    request.AddHeader("authorization", authorizationValue);
+                    request.AddHeader("cache-control", "no-cache");
+                    request.AddParameter("application/json", "{ \"roles\": [ \"rol_fWiLOHdB4uUAg3Fq\" ] }", ParameterType.RequestBody);
+                    response = client.Execute(request);
+                }
             }
             var model = await _userRepository.GetAllUsers();
             return View(model);
