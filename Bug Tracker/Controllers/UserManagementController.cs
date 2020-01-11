fd7799cb2b73b246fabf76a18edae78c204350fe
SALAZAR <jonathan-ol-salazar@outlook.com>
2020-01-11 23:16:20 +1000

    Can now show user role

--- Bug Tracker/Controllers/UserManagementController.cs
+++ Bug Tracker/Controllers/UserManagementController.cs
@@ -28,86 +28,59 @@ namespace Bug_Tracker.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            // var model = await _userRepository.GetAllUsers();
-
             // Reset MongoDB 'Users' collection
             await _userRepository.ResetUsers();
 
-            if (User.Identity.IsAuthenticated)
+            // ACCESS TOKEN FOR AUTH0 MANAGEMENT API
+
+            var client = new RestClient("https://wussubininja.au.auth0.com/oauth/token");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request.AddParameter("application/x-www-form-urlencoded", "grant_type=client_credentials&client_id=LZ1ZnJCpRTSZB4b2iET97KhOajNiPyLk&client_secret=6Actr7Xa1tNRC6370iM6rzD68Wbpq8UCurK3QbtBiRRAUZqheOwFzDspQkZ2-7QJ&audience=https://wussubininja.au.auth0.com/api/v2/", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            // Parsing into JSON 
+            var response2dict = JObject.Parse(response.Content);
+            // Retrieving Access Token
+            var Auth0ManagementAPI_AccessToken = response2dict.First.First.ToString();
+
+
+
+
+            // GETTING ALL USERS
+            string baseURL = "https://wussubininja.au.auth0.com/api/v2/users";
+            string authorizationValue = "Bearer " + Auth0ManagementAPI_AccessToken;
+            // Endpoint to get user role
+            client = new RestClient(baseURL);
+            request = new RestRequest(Method.GET);
+            // Add Auth0 Management API Access Token 
+            request.AddHeader("authorization", authorizationValue);
+            response = client.Execute(request);
+
+
+            JArray usersAuth0 = JArray.Parse(response.Content);
+            var Users = new List<User>();
+
+            foreach (var userAuth0 in usersAuth0)
             {
-                string accessToken = await HttpContext.GetTokenAsync("access_token");
-
-                // if you need to check the Access Token expiration time, use this value
-                // provided on the authorization response and stored.
-                // do not attempt to inspect/decode the access token
-                DateTime accessTokenExpiresAt = DateTime.Parse(
-                    await HttpContext.GetTokenAsync("expires_at"),
-                    CultureInfo.InvariantCulture,
-                    DateTimeStyles.RoundtripKind);
-
-                string idToken = await HttpContext.GetTokenAsync("id_token");
-
-                // Now you can use them. For more info on when and how to use the
-                // Access Token and ID Token, see https://auth0.com/docs/tokens
-
-                // Reading JWT idToken
-                var handler = new JwtSecurityTokenHandler();
-                var token = handler.ReadJwtToken(idToken);
-                string user_ID = token.Payload.Sub;
-
-
-                // ACCESS TOKEN FOR AUTH0 MANAGEMENT API
-
-                var client = new RestClient("https://wussubininja.au.auth0.com/oauth/token");
-                var request = new RestRequest(Method.POST);
-                request.AddHeader("content-type", "application/x-www-form-urlencoded");
-                request.AddParameter("application/x-www-form-urlencoded", "grant_type=client_credentials&client_id=LZ1ZnJCpRTSZB4b2iET97KhOajNiPyLk&client_secret=6Actr7Xa1tNRC6370iM6rzD68Wbpq8UCurK3QbtBiRRAUZqheOwFzDspQkZ2-7QJ&audience=https://wussubininja.au.auth0.com/api/v2/", ParameterType.RequestBody);
-                IRestResponse response = client.Execute(request);
-
-                // Parsing into JSON 
-                var response2dict = JObject.Parse(response.Content);
-                // Retrieving Access Token
-                var Auth0ManagementAPI_AccessToken = response2dict.First.First.ToString();
-
-
-
-
-                // GETTING ALL USERS
-                string baseURL = "https://wussubininja.au.auth0.com/api/v2/users";
-                string authorizationValue = "Bearer " + Auth0ManagementAPI_AccessToken;
-                // Endpoint to get user role
+                // GETTING ROLES ASSIGNED TO USER FROM AUTH0
+                baseURL = "https://wussubininja.au.auth0.com/api/v2/users/" + userAuth0.SelectToken("user_id").ToString() + "/roles";
                 client = new RestClient(baseURL);
-                request = new RestRequest(Method.GET);
-                // Add Auth0 Management API Access Token 
-                request.AddHeader("authorization", authorizationValue);
                 response = client.Execute(request);
+                JArray userRolesAuth0 = JArray.Parse(response.Content);
+                var document = new User();
 
-                var content = response.Content;
-                JArray contentArray = JArray.Parse(content);
-                var Users = new List<User>();
+                document.ID = userAuth0.SelectToken("user_id").ToString();
+                document.UserName = userAuth0.SelectToken("name").ToString();
+                document.Email = userAuth0.SelectToken("email").ToString();
+                document.Role = userRolesAuth0.First.SelectToken("name").ToString();
 
-                foreach (var userAuth0 in contentArray)
-                {
-                    var document = new User();
-
-                    document.ID = userAuth0.SelectToken("user_id").ToString();
-                    document.UserName = userAuth0.SelectToken("name").ToString();
-                    document.Email = userAuth0.SelectToken("email").ToString();
-                    //document.Id = userAuth0.SelectToken("email").ToString();
-                    //  document.Role = userAuth0.SelectToken("app_metadata").SelectToken("roles").ToString();
-
-                    Users.Add(document);
-                }
-
-                await _userRepository.AddUsers(Users);
-
-
-
-
-
-
+                Users.Add(document);
             }
 
+            // Add all users from Auth0 to MongoDB 'Users' collection
+            await _userRepository.AddUsers(Users);
+            // Get all users from 'Users' collection and use a model for 'Index' view
             var model = await _userRepository.GetAllUsers();
 
             return View(model);
