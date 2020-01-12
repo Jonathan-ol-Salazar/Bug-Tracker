14aecc0e4b9eddb129dae2d42a252b9f69f7ed91
SALAZAR <jonathan-ol-salazar@outlook.com>
2020-01-12 12:28:22 +1000

    Added bootstrap template to Users table

--- Bug Tracker/Controllers/HomeController.cs
+++ Bug Tracker/Controllers/HomeController.cs
@@ -60,12 +60,12 @@ namespace Bug_Tracker.Controllers
                 request.AddHeader("content-type", "application/x-www-form-urlencoded");
                 request.AddParameter("application/x-www-form-urlencoded", "grant_type=client_credentials&client_id=LZ1ZnJCpRTSZB4b2iET97KhOajNiPyLk&client_secret=6Actr7Xa1tNRC6370iM6rzD68Wbpq8UCurK3QbtBiRRAUZqheOwFzDspQkZ2-7QJ&audience=https://wussubininja.au.auth0.com/api/v2/", ParameterType.RequestBody);
                 IRestResponse response = client.Execute(request);
-
+                
                 // Parsing into JSON 
                 var response2dict = JObject.Parse(response.Content);
                 // Retrieving Access Token
                 var Auth0ManagementAPI_AccessToken = response2dict.First.First.ToString();
-
+               
 
                 // GETTING ROLES ASSIGNED TO USER FROM AUTH0
                 // Format: https://wussubininja.au.auth0.com/api/v2/users/USER_ID/roles
