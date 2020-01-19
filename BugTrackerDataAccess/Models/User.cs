c53de121c457f25d97bd9665e94dd045ed62ca65
SALAZAR <jonathan-ol-salazar@outlook.com>
2020-01-19 14:55:30 +1000

    Can now display selected project name dynamically

--- BugTrackerDataAccess/Models/User.cs
+++ BugTrackerDataAccess/Models/User.cs
@@ -54,61 +54,6 @@ namespace BugTrackerDataAccess.Models
 
         //public string ProfileImage { get; set; }
 
-
-
-        //public void AddorRmove(string AddorRemove, Project project, User selectedUser)
-        //{
-        //    var userFromDb = await _userRepository.GetUser(selectedUser.ID);
-        //    if (userFromDb == null)
-        //    {
-        //        return new NotFoundResult();
-        //    }
-
-        //    selectedUser.Id = userFromDb.Id;
-        //    selectedUser.Projects = userFromDb.Projects;
-        //    if (AddorRemove == "Add")
-        //    {
-        //        selectedUser.Projects.Add(project.IDCode);
-        //    }
-        //    else
-        //    {
-        //        selectedUser.Projects.Remove(project.IDCode);
-        //    }
-
-        //    object Projects = "{ \"projects\": [\"" + string.Join(",", selectedUser.Projects) + "\"]}}";
-        //    if (AddorRemove == "Remove")
-        //    {
-        //        if (selectedUser.Projects.Count == 0)
-        //        {
-        //            Projects = "{ \"projects\": {}}}";
-        //        }
-        //    }
-
-
-        //    // Use Auth0 API to add PROJECT to user metadata
-        //    string authorizationValue = "Bearer " + Auth0ManagementAPI_AccessToken;
-        //    string baseURL = "https://wussubininja.au.auth0.com/api/v2/users/" + selectedUser.ID;
-        //    client = new RestClient(baseURL);
-        //    request = new RestRequest(Method.PATCH);
-        //    request.AddHeader("authorization", authorizationValue);
-        //    request.AddHeader("content-type", "application/json");
-        //    request.AddParameter("application/json", "{\"app_metadata\": " + Projects, ParameterType.RequestBody);
-        //    response = client.Execute(request);
-
-
-        //}
-
-
-
-
-
-
-
-
-
-
-
-
     }
 
 
