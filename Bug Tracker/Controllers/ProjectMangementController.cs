03595364b28e15ecf5ba81fb3f16901935ce13fb
SALAZAR <jonathan-ol-salazar@outlook.com>
2020-02-02 22:49:12 +1000

    Fix bug with deleting issues - user issue list was being deleted if issue was from same project

--- Bug Tracker/Controllers/ProjectMangementController.cs
+++ Bug Tracker/Controllers/ProjectMangementController.cs
@@ -235,7 +235,7 @@ namespace Bug_Tracker.Controllers
 
                 Project.Issues = projectIssues;
 
-                await _projectRepository.Update(Project);
+                //await _projectRepository.Update(Project);
 
 
                 // Delete issues from users
@@ -252,7 +252,7 @@ namespace Bug_Tracker.Controllers
 
                             foreach (var issue in User.Issues)
                             {
-                                if (!issue.Contains(Issue.ProjectIDCode))
+                                if (!issue.Contains(Issue.ProjectIDCode + "-" + issueSelected))
                                 {
                                     newIssueList.Add(issue);
                                 }
